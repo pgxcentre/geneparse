1a2662317212775f852aef5e847c67a369ff3026
Marc André Legault <legaultmarc@gmail.com>
2017-03-17 14:00:50 -0400

    Refactored container tests so that they can be reused easily.

--- geneparse/tests/test_plink.py
+++ geneparse/tests/test_plink.py
@@ -6,7 +6,7 @@ import os
 import unittest
 import logging
 
-from . import truth
+from .generic_tests import TestContainer
 from .. import plink
 
 
@@ -19,98 +19,7 @@ PLINK_PREFIX = os.path.abspath(os.path.join(
 ))
 
 
-class TestPlink(unittest.TestCase):
-    def test_iter_variants(self):
-        """Test that all variants are iterated over"""
-        # We expect the variants in the same order as the BIM.
-        expected = [
-            truth.variants["rs785467"],
-            truth.variants["rs146589823"],
-            truth.variants["subal_3_rs9628434"],
-            truth.variants["subal_2_rs9628434"],
-            truth.variants["rs140543381"],
-        ]
-
-        with plink.PlinkReader(PLINK_PREFIX) as f:
-            for i, v in enumerate(f.iter_variants()):
-                self.assertEqual(v, expected[i])
-
-        self.assertEqual(i, len(expected) - 1)  # All variants were iterated.
-
-    def test_iter_genotypes(self):
-        """Test that the genotypes are read correctly"""
-        with plink.PlinkReader(PLINK_PREFIX) as f:
-            for g in f.iter_genotypes():
-                expected = truth.genotypes[truth.variant_to_key[g.variant]]
-                self.assertEqual(expected, g)
-
-    def test_multiallelic_identifier(self):
-        """Test that the multiallelic flag gets set when iterating"""
-        with plink.PlinkReader(PLINK_PREFIX) as f:
-            for g in f.iter_genotypes():
-                expected = truth.genotypes[truth.variant_to_key[g.variant]]
-                self.assertEqual(expected.multiallelic, g.multiallelic)
-
-    def test_get_biallelic_variant(self):
-        """Test simplest possible case of variant accession."""
-        v = truth.variants["rs785467"]
-        expected = truth.genotypes["rs785467"]
-        with plink.PlinkReader(PLINK_PREFIX) as f:
-            g = f.get_variant_genotypes(v)[0]
-            self.assertEqual(expected, g)
-
-    def test_get_multiallelic_variant_by_locus(self):
-        """Test getting a multiallelic variant using a locus."""
-        v = truth.variants["locus_rs9628434"]
-        # Coded allele to expected genotypes.
-        expected = {
-            "T": truth.genotypes["subal_2_rs9628434"],
-            "A": truth.genotypes["subal_3_rs9628434"]
-        }
-        with plink.PlinkReader(PLINK_PREFIX) as f:
-            for g in f.get_variant_genotypes(v):
-                self.assertEqual(
-                    expected[g.coded], g
-                )
-                del expected[g.coded]
-
-        self.assertEqual(len(expected), 0)
-
-    def test_get_multiallelic_variant_by_specific(self):
-        """Find a biallelic variant at a multiallelic locus."""
-        # subal 2 is the G/T variant.
-        v = truth.variants["subal_2_rs9628434"]
-        expected = truth.genotypes["subal_2_rs9628434"]
-        with plink.PlinkReader(PLINK_PREFIX) as f:
-            g = f.get_variant_genotypes(v)
-            self.assertEqual(len(g), 1)
-
-            g = g[0]
-            self.assertEqual(expected, g)
-
-    def test_get_multiallelic_variant_by_unavailable(self):
-        """Test asking for an unavailable biallelic variant at a multiallelic
-        locus.
-
-        """
-        v = truth.variants["subal_1_rs9628434"]
-        with plink.PlinkReader(PLINK_PREFIX) as f:
-            self.assertEqual([], f.get_variant_genotypes(v))
-
-    def test_get_multiallelic_variant_by_multiallelic(self):
-        """Test asking for a multiallelic variant."""
-        v = truth.variants["rs9628434"]
-
-        expected = {
-            "T": truth.genotypes["subal_2_rs9628434"],
-            "A": truth.genotypes["subal_3_rs9628434"],
-        }
-        with plink.PlinkReader(PLINK_PREFIX) as f:
-            for g in f.get_variant_genotypes(v):
-                # Find the right expected genotypes.
-                self.assertEqual(
-                    expected[g.coded], g
-                )
-                del expected[g.coded]
-
-        self.assertEqual(len(expected), 0)
+class TestPlink(TestContainer, unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.reader_f = lambda x: plink.PlinkReader(PLINK_PREFIX)
